83e979136d5156e76c6fa55f9d2d4e2a1e4e54d6
snowsoft <abdulkadirkar@gmail.com>
2016-12-05 00:41:46 +0300

    -
    -

---
+++ src/Snowsoft/FormValidate/Validate.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Abdulkadir
+ * Date: 4.12.2016
+ * Time: 23:36
+ */
+
+namespace Snowsoft\FormValidate;
+
+
+class Validate
+{
+
+    public static function get()
+    {
+
+    }
+
+    public static function post()
+    {
+
+    }
+
+
+
+}
